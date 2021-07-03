cd6aaf792abaec142b97daf940a271dd66f65b9d
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2021-07-02 17:13:36 -0700

    Add Rocket

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, routes};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/test", routes![db_test])
+        .launch()
+        .await;
 }
