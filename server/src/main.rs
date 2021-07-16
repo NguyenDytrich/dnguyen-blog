2c0b187a73ae9a5ec4dbc4e0c24d773e702e11cc
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2021-07-16 10:55:22 -0700

    Add query parameters to /posts

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,10 +7,24 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-/// Retreives the 10 most recent posts in JSON format
-#[get("/recent")]
+/// By default, retrieve 10 most recent posts
+#[get("/posts")]
 async fn recent_posts() -> Option<String> {
     let post = match retrieve_recent(10).await {
+        Ok(r) => match serde_json::to_string(&r) {
+            Ok(v) => Some(v),
+            Err(_) => None,
+        },
+        Err(_) => None,
+    };
+
+    return post;
+}
+
+/// Retreives a number of recent posts in JSON format
+#[get("/posts?<count>")]
+async fn recent_posts_count(count: i64) -> Option<String> {
+    let post = match retrieve_recent(count).await {
         Ok(r) => match serde_json::to_string(&r) {
             Ok(v) => Some(v),
             Err(_) => None,
@@ -26,8 +40,7 @@ async fn main() {
     dotenv().ok();
 
     let _server = rocket::build()
-        .mount("/", routes![index])
-        .mount("/posts", routes![recent_posts])
+        .mount("/", routes![index, recent_posts, recent_posts_count])
         .launch()
         .await;
 }
