5f12fe2534fb37066d6de6d99c9cd66a1f29e156
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2021-07-23 14:40:27 -0700

    add user and role relations to database script

--- server/migrations/initialize.sql
+++ server/migrations/initialize.sql
@@ -10,8 +10,18 @@ CREATE TABLE IF NOT EXISTS blog_posts (
 
 CREATE TABLE IF NOT EXISTS users (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-	email VARCHAR(255) NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
 	password_hash VARCHAR(255) NOT NULL,
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 	last_login TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_roles (
+	user_id UUID references users(id),
+	role INT references roles(id)
 );
